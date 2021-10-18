fae5498b22835c86444bfbc99ebfe1fb4640ad8d
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-10-19 00:35:41 +0530

    ADD: config for plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,24 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "the-mark-gatsby",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    }
+  ],
 };
