cf123f00258eaf1d5147809d47f3f55e0a3bae10
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-10-21 01:07:00 +0530

    ADD: list of posts to homepage using GraphQL page query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,55 @@
 import * as React from "react";
+import { graphql } from 'gatsby';
 import Header from '../components/Header';
 
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+
   return (
     <>
       <Header />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontFamily: 'avenir'
+      }}>
+        {
+          edges.map(edge => {
+            const { frontmatter } = edge.node
+            return (
+              <div
+                key={frontmatter.path}
+                style={{ marginBottom: '1rem' }}
+              >
+                {frontmatter.title}
+              </div>
+            )
+          })
+        }
+      </div>
     </>
   );
 };
+
+export const query = graphql`
+  query HomepageQuery {
+    allMarkdownRemark(
+      sort: {order: DESC, fields: [frontmatter___date]}
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    } 
+  }
+`;
 
 export default IndexPage;
